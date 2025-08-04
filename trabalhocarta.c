5c27e351f520077cf5bffc6a4f418e69950c1cd8
yurizx15 <brasilyuri6@gmail.com>
2025-08-04 19:18:17 +0000

    criei as informacoes

---
+++ trabalhocarta.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+  int main(){
+    //Declarando cartas 1
+char estado1;
+char codigo1{4};
+char nomeCidade1{30};
+int populacao1;
+float area1;
+float pib1;
+int pontosTuristico1;
+
+
+
