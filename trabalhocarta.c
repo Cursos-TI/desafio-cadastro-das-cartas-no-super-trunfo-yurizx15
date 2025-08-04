b37209822424648e94f442b05582081ab08f9005
yurizx15 <brasilyuri6@gmail.com>
2025-08-04 19:36:03 +0000

    comecando o trabalho

--- trabalhocarta.c
+++ trabalhocarta.c
@@ -2,8 +2,8 @@
   int main(){
     //Declarando cartas 1
 char estado1;
-char codigo1{4};
-char nomeCidade1{30};
+char codigo1[4];
+char nomeCidade1[30];
 int populacao1;
 float area1;
 float pib1;
@@ -11,8 +11,8 @@ int pontosTuristico1;
 
    //Declarando cartas 2
 char estado2;
-char codigo2{5};
-char nomeCidade{30};
+char codigo2[4];
+char nomeCidade[30];
 int populacao2;
 float area2;
 float pib2;
@@ -20,4 +20,6 @@ int pontosTuristico2;
 
    //Entrada dos dados carta1
 
+printf("Digite os dados da Carta1: \n");
 
+  
